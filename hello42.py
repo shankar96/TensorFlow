bead51575338b0e15ebec7da54234a4ba6b4a58c
shankar <shankarkumarchaudhary96@gmail.com>
2016-07-01 09:57:38 +0000

    first commit

--- hello42.py
+++ hello42.py
@@ -1,8 +1,13 @@
 import tensorflow as tf
+import numpy as np
+
+a=np.random.rand(5).astype(np.float32)
+
 hello = tf.constant('Hello, TensorFlow!')
 sess = tf.Session()
 print(sess.run(hello))
-
+print a
 a = tf.constant(10)
 b = tf.constant(32)
 print(sess.run(a + b))
+print 'https://github.com/shankar96/TensorFlow.git'
